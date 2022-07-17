391719f906439e73d06a0da6c1346abba1387d73
Sepanta Zeighami <sepzeighami@gmail.com>
2022-07-17 13:24:02 -0700

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -14,7 +14,7 @@ def call_tree(config, DB, queries, test_queries, res, test_res):
     in_test_res = in_test_res.reshape((-1, config['out_dim']))
     in_res = in_res.reshape((-1, config['out_dim']))
 
-    my_model, _, processes = build_tree(config['depth'], config['no_filters'], config['in_dim'], queries, in_res, test_queries, in_test_res, processes, config['exp_name'], "", 0, 0, config['no_processes'], config['path_to_NeuroDB'])
+    my_model, _, processes = build_tree(config['depth'], config['no_filters'], config['in_dim'], queries, in_res, test_queries, in_test_res, processes, config['exp_name'], "", 0, 0, config['no_processes'], config['path_to_neurosketch'])
 
     if len(processes) > 0:
         for p in processes:
@@ -24,7 +24,7 @@ def call_tree(config, DB, queries, test_queries, res, test_res):
 def test_model(config, test_qs, test_res):
     np.savetxt(config['exp_name']+"_queries.txt", test_qs, delimiter=",")
     np.savetxt(config['exp_name']+"_res.txt", test_res, delimiter=",")
-    c_call = config["path_to_NeuroDB"]+"/run_model "+str(config['no_filters'])+" " + str(config['in_dim'])+" " + str(config['out_dim']) +' ' + str(config['depth'])+ ' ' + config['exp_name'] + ' ' + config['exp_name'] + " 10000 1"
+    c_call = config["path_to_neurosketch"]+"/run_model "+str(config['no_filters'])+" " + str(config['in_dim'])+" " + str(config['out_dim']) +' ' + str(config['depth'])+ ' ' + config['exp_name'] + ' ' + config['exp_name'] + " 10000 1"
     print("Testing model")
     os.system(c_call)
 
