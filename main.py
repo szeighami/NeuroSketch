0108a0c195383ff24c196c246d0ed206ad256083
Sepanta Zeighami <sepzeighami@gmail.com>
2022-07-17 13:20:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,8 @@ with open(sys.argv[1], 'r') as f:
 start = time.time()
 
 print("Starting data collection")
-if config["query_type"] == "NN" or config["query_type"] == "dist_NN":
 
-    DB, queries, test_queries, res, test_res = get_nn_data(config["n"], config["in_dim"], config["query_type"] == "dist_NN", config["train_size"], config["test_size"], config["k_th"], config["data_loc"], config["MAX_VAL"], config["no_threads"])
-elif config["query_type"] == "RAQ":
-
-    DB, queries, test_queries, res, test_res = get_raq_data(config["n"], config["db_dim"], config["in_dim"], config["train_size"], config["test_size"], config['q_range'], config['agg_type'], config['MAX_VAL'], config['data_loc'], config['db_ag_col'], config["train_data_size"], config["active_pred_dim"], config["active_pred_dim_pairs"], config["with_angle"], config["no_threads"])
-else:
-    raise ValueError("ValueError exception thrown")
-
+DB, queries, test_queries, res, test_res = get_raq_data(config["n"], config["db_dim"], config["in_dim"], config["train_size"], config["test_size"], config['q_range'], config['agg_type'], config['MAX_VAL'], config['data_loc'], config['db_ag_col'], config["train_data_size"], config["active_pred_dim"], config["active_pred_dim_pairs"], config["with_angle"], config["no_threads"])
 
 end = time.time()
 print("Data collection took {:.2f}s".format(end-start))
